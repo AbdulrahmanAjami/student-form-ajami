be472184828f2e4ae1ff50086eea897ea9154e5a
abdulrahmanAjami <abood.ajami2014@gmail.com>
2026-07-15 15:35:26 +0300

    fill student form

--- src/main/kotlin/StudentForm.kt
+++ src/main/kotlin/StudentForm.kt
@@ -1,6 +1,6 @@
 data class StudentForm(
     var name: String = "Abdulrahman",
-    var lastName: String = "Ajami",
+    var lastName: String = "Ajamiii",
     var age: String = "22",
     var university: String = "Hashemite University",
 )
